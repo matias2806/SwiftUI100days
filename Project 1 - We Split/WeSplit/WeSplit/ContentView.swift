bc3b9aee350323018a541f63d95c90b16c30a93c
matiasPalmieri <86312764+matiasPalmieri@users.noreply.github.com>
2023-06-06 19:06:52 -0300

    we split initial commit

---
+++ Project 1 - We Split/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Matias Palmieri on 06/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world ss!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project 1 - We Split/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Matias Palmieri on 06/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
