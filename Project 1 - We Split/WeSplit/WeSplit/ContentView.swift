3008cbb6f713024dc3f648a388c53689a1e8819d
matiasPalmieri <86312764+matiasPalmieri@users.noreply.github.com>
2023-06-09 19:59:54 -0300

    P1- Now we can se the amount more tip and refactoring code

--- Project 1 - We Split/WeSplit/WeSplit/ContentView.swift
+++ Project 1 - We Split/WeSplit/WeSplit/ContentView.swift
@@ -14,6 +14,10 @@ struct ContentView: View {
     @State private var tipPercentage = 20
     let tipPercentages = [10, 15, 20, 25, 0]
     
+    var currency: FloatingPointFormatStyle<Double>.Currency {
+        return .currency(code: Locale.current.currency?.identifier ?? "USD")
+    }
+    
     var totalPerPerson: Double {
         let peopleCount = Double(numberOfPeople + 2)
         let tipSelection = Double(tipPercentage)
@@ -51,9 +55,33 @@ struct ContentView: View {
                 } header: {
                     Text("How much tip do you want to leave?")
                 }
+//                Section {
+//                    Picker("Tip percentage", selection: $tipPercentage) {
+//                        ForEach(0 ..< 51) {
+//                            if $0 % 5 == 0{
+//                                Text("\($0)%")
+//                            }
+//                        }
+//                    }
+//                    .pickerStyle(.navigationLink)
+//                } header: {
+//                    Text("How much tip do you want to leave?")
+//                }
+                
+                
+                Text("Result").foregroundColor(.red)
+                
                 
                 Section {
-                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    Text(totalPerPerson, format: currency)
+                } header: {
+                    Text("Amount per person")
+                }
+                
+                Section {
+                    Text(totalPerPerson * Double(numberOfPeople + 2), format: currency)
+                } header: {
+                    Text("Amount more tip")
                 }
             }.navigationTitle("We Split")
                 .toolbar {
