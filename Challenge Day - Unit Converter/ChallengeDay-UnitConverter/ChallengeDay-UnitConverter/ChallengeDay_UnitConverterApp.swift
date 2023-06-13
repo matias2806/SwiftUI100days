cbb35d1f12c589a5655f4220858c9f51d8ba8b39
matiasPalmieri <86312764+matiasPalmieri@users.noreply.github.com>
2023-06-13 16:22:08 -0300

    new project structure - unit converter

---
+++ Challenge Day - Unit Converter/ChallengeDay-UnitConverter/ChallengeDay-UnitConverter/ChallengeDay_UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeDay_UnitConverterApp.swift
+//  ChallengeDay-UnitConverter
+//
+//  Created by Matias Palmieri on 13/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeDay_UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
