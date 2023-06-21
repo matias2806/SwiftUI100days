f4224e2d8dae25fc6adf6ab7e3936f680bd2bc77
matiasPalmieri <86312764+matiasPalmieri@users.noreply.github.com>
2023-06-21 18:25:55 -0300

    initial commit project 2

---
+++ Project 2 - Guess The Flag/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Matias Palmieri on 21/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
